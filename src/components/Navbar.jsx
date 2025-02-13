2259af825624f6d20d4568a10a62d4c66a1dc248
M.tayyab <tayyabhamza173@gmail.com>
2025-02-13 18:47:37 +0500

    updated website name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <span class="ml-3 text-xl">Single Page Application</span>
+      <span class="ml-3 text-xl">Spa</span>
     </a>
     <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
       <Link to='/' class="mr-5 hover:text-gray-900">Home</Link>
